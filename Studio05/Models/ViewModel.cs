e3350ca8fa646172973e23c9d3eab34a403f77c3
duanyinglin1 <wobushidyl@gmail.com>
2023-09-03 22:44:48 +1000

    Add project files.

---
+++ Studio05/Models/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studio05.Models
+{
+    public class ViewModel
+    {
+    }
+
+    public class FormOneViewModel {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set;}
+
+        public string LastName { get; set;}
+
+    }
+}
