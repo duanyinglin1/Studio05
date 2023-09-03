3dd21f97c6b8de7f15484407d52aa72d98f12585
duanyinglin1 <wobushidyl@gmail.com>
2023-09-03 23:26:29 +1000

    Adding Async and deffer html and js file, to check the differnce.

---
+++ Studio05/Scripts/a-d.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+
+    let h1 = document.querySelector("h1");
+    for (let i = 0; i < 1000; i++) {
+        h1.textContent += String.fromCharCode(Math.floor(Math.random() * 60) + 65);
+    }
+
+});
+
+
+let str = "";
+for (let i = 0; i < 5000; i++) {
+    h1.textContent += String.fromCharCode(Math.floor(Math.random() * 60) + 65);
+}
+console.log("Test about async and deffer");
